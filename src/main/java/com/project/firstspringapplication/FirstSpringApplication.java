d2e1ee6f49965a8572856d2943325fa585a5f04f
unknown <mayurpawar8691@gmail.com>
2022-08-12 10:50:11 +0530

    Added: Creat Hello world Spring App

---
+++ src/main/java/com/project/firstspringapplication/FirstSpringApplication.java
@@ -0,0 +1,14 @@
+package com.project.firstspringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz!");
+        SpringApplication.run(FirstSpringApplication.class, args);
+    }
+
+}
